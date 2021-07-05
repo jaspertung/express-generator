72c54994c6c1350f548e1240662a999d6262535c
Jasper <jtunggsp@gmail.com>
2021-07-05 11:19:12 -0700

    sign and issue json web tokens, update passport to use JWT strategy, grant access to GET endpoints for unauth users and other endpoints for auth users

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session')
-const FileStore = require('session-file-store')(session) //2 params: require function returning another function as its return value and calling the return function with 2nd param (session)
 const passport = require('passport');
-const authenticate = require('./authenticate');
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,7 +12,7 @@ const partnerRouter = require('./routes/partnerRouter')
 const promotionRouter = require('./routes/promotionRouter')
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/nucampsite';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
@@ -38,66 +35,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321')); //arg: secret key --------replaced with Express sessions
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false, //when new session created but no updates, then won't save because will be empty session (prevents empty session files and cookies)
-  resave: false, //won't resave for new requests for the session since no updates (marks session as active so won't be deleted)
-  store: new FileStore() //save in hard disk instead of application memory
-}))
 //don't need authentication for ^
 
 app.use(passport.initialize());
-app.use(passport.session());
 //moved to above auth function to allow clients to create new accounts and route logged out/unauthenticated users back to index
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-//so authentication starts here to serve static files-- creating custom middleware function named auth
-function auth(req, res, next) { //all express middleware functions require req and res objects as params, next is optional
-  //console.log(req.headers)
-  console.log(req.user)
-  //if (!req.signedCookies.user) { //signedCookies: auto parses cookie from request, if not properly signed then will return false -------replaced with Sessions
-    //if no signedCookies.user or false, client hasn't been authenticated
-  
-  if (!req.user) {
-    //const authHeader = req.headers.authorization
-    //if (!authHeader) { //if null, then didn't get any authentication info (no username/pw) -----removed when added user router
-    const err = new Error('You are not authenticated!')
-    //res.setHeader('WWW-Authenticate', 'Basic') //lets client know that server is requesting auth and auth method being requested is Basic ------removed when added user router
-    err.status = 401 //error code for when auth info not provided
-    return next(err) //pass err message to express
-
-    //sends error message back and requests clients credentials
-    //if there is an auth header then decode username and pw info, then parse into array ['admin', 'password']
-    // const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':') //Buffer: global in NodeJS (don't need to be required)
-    // const user = auth[0]
-    // const pass = auth[1]
-    // //basic validation
-    // if (user === 'admin' && pass === 'password') { //if true then pass to next middleware function
-    //   //set up cookie
-    //   //res.cookie('user', 'admin', {signed: true}) //creates new cookie passing in name of cookie (user), value of name property (admin), and optional object containing config values (let express know to use cookie parser to create signed cookie)  -------replaced with Sessions
-    //   req.session.user = 'admin'
-    //   return next() //authorized
-    // } else {
-    //   const err = new Error('You are not authenticated!')
-    //   res.setHeader('WWW-Authenticate', 'Basic')
-    //   err.status = 401
-    //   return next(err)
-    // } ------removed when added user router
-  } else { //if there is a signed cookie
-    //if (req.signedCookies.user === 'admin') { -------replaced with Sessions
-    // if (req.session.user === 'authenticated') {
-    //   return next()
-    // } else {
-    //   const err = new Error('You are not authenticated!')
-    //   err.status = 401
-    //   return next(err)
-    // }
-    return next()
-  }
-}
-app.use(auth)
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const User = require('../models/user')
 const passport = require('passport');
+const authenticate = require('../authenticate');
 
 const router = express.Router();
 
@@ -33,9 +34,10 @@ router.post('/signup', (req, res) => {
 
 //check if user is already logged in (already tracking authenticated session)
 router.post('/login', passport.authenticate('local'), (req, res) => {
+  const token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 //log out the user (user GET because client is sending info to server)
